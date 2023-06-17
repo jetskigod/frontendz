42d9ad13c4a9f0b9f34d358127de8e09819a8ed8
jetskigod <jetskiyoungsins@gmail.com>
2023-06-17 22:06:02 +0700

    jetupdate

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function index() {
   <header>
 <nav className="navbar navbar-expand-lg bg-danger">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">JETZ</a>
+    <a className="navbar-brand" href="#">JET</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
