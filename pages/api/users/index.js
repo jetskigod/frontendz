1f40483ed31a3c18d93c95c71e26899413f0d8c2
jetskigod <jetskiyoungsins@gmail.com>
2023-06-28 09:49:34 +0700

    env

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
   });
   export default function handler(req, res) {
 // simple query
