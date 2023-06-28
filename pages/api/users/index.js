325d1b21e6fadd09400103692b13e1635ce645fa
jetskigod <jetskiyoungsins@gmail.com>
2023-06-28 09:47:37 +0700

    db

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -3,9 +3,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'db_users'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_PASS,
   });
   export default function handler(req, res) {
 // simple query
