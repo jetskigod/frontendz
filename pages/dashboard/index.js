879ce7d8a8c4839d392168e61084bb216d960ddf
jetskigod <jetskiyoungsins@gmail.com>
2023-07-26 09:24:48 +0700

    955251

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,19 +1,79 @@
+
 import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/users')
+  const posts = await res.json()
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default function Component({ posts }) {
   const { data: session } = useSession()
+
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        {session.user.fname}  {session.user.lname}<br />
-        <button onClick={() => signOut()}>Sign out</button>
+
+<nav class="navbar navbar-dark bg-danger">
+  <div className="container-fluid">
+  <div className="col">
+  <div align="right"> Signed in as {session.user.fname} {session.user.lname} <button  className="btn btn-danger" onClick={() => signOut()}>Sign out</button> </div>
+  </div>
+  </div>
+</nav>
+<br></br>
+
+        <div className="container">
+          <div className="row">
+          </div>
+          <div align="right">  <button className="btn btn-success text-n">Add Data</button> {/* ปุ่ม Delete */} </div>
+          <br></br>
+          <div className="row">
+            <div className="col">
+              <table className="table table-striped">
+                <thead>
+                  <tr className="bg-warning">
+                    <th>No</th>
+                    <th>Student ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Password</th>
+                    <th>status</th>
+                    <th>edit/delete</th> {/* เพิ่มคอลัมน์ Action */}
+                  </tr>
+                </thead>
+                <tbody>
+                  {posts.users.map((post, i) => (
+                    <tr key={post.id}>
+                      <td>{i+1}</td>
+                      <td>{post.studentid}</td>
+                      <td>{post.firstname}</td>
+                      <td>{post.lastname}</td>
+                      <td>{post.password}</td>
+                      <td>{post.status}</td>
+                      <td>
+                        <button className="btn btn-warning">Edit</button> {/* ปุ่ม Edit */}
+                        <button className="btn btn-danger">Delete</button> {/* ปุ่ม Delete */}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
+
   return (
     <>
-      Not signed ins <br />
+      Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,8 @@ export default function index() {
         </li>
       </ul>
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-dark" type="submit">ค้นหา</button>
+        <button className="btn btn-outline-dark" type="submit"><a href="./dashboard" target="_blank">Go !</a></button>
+
       </form>
     </div>
   </div>
