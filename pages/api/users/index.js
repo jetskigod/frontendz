d8a234b586953721105d142c48b8d8b6449ed28f
jetskigod <jetskiyoungsins@gmail.com>
2023-08-16 14:52:35 +0700

    day

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -9,6 +9,14 @@ const connection = mysql.createConnection({
     database: process.env.DB_DATABASE
   });
   export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    
     const { studentid,firstname,lastname,username,password,status} = req.body
     if (req.method === 'GET'){
       connection.query(
@@ -21,7 +29,7 @@ const connection = mysql.createConnection({
       const result = connection.query("INSERT INTO tbl_users SET ?",{
         studentid,firstname,lastname,username,password,status
       });
-      return res.status(200).json({...req.body, id: result.insertId});
+      return res.status(200).json({"status":"ok", "message":req.body, "id": result.insertId});
 
     }else if (req.method === 'PUT'){
 
@@ -30,7 +38,7 @@ const connection = mysql.createConnection({
           req.body,
           req.body.id,
         ]);
-        return res.status(200).json({...req.body, id: result.insertId});
+        return res.status(200).json({...req.body,id: result.insertId});
       } catch (error){
         return res.status(500).json({ message: error.message});
       }
